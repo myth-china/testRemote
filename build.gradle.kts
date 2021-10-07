c74c7572f9b251efc5e57ac0bafdcc5bbadd7264
wilfred.y <wilfred.y@binance.com>
2021-10-07 17:20:42 +0800

    Add project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "com.wilfred"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ src/main/kotlin/com/wilfred/testremote/TestRemote.kt
@@ -0,0 +1,8 @@
+package com.wilfred.testremote
+
+class TestRemote {
+
+    fun print() {
+        println("TestRemote")
+    }
+}
