6b95eaccdd81b635da2d74d5a9b32e7375f533ff
wilfred.y <wilfred.y@binance.com>
2021-10-09 16:25:31 +0800

    Delete java-modular config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,16 @@
 plugins {
     kotlin("jvm") version "1.5.31"
-    `java-modular-project`
 }
 
-group = "com.wilfred"
-version = "1.0-SNAPSHOT"
+group = "com.wilfred.testremote"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
     mavenLocal()
-}
-
-buildscript {
-    repositories {
-        mavenCentral()
-        mavenLocal()
-    }
-
-    dependencies {
-        classpath("com.yanwq.plugin:java-modular:1.0-SNAPSHOT")
+    maven {
+        url = uri("http://nexus.k8s.qa1fdg.net/repository/maven-snapshots/")
+        isAllowInsecureProtocol = true
     }
 }
 
--- testRemoteA/build.gradle.kts
+++ testRemoteA/build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
     kotlin("jvm")
-    `java-modular-project`
+    `maven-publish`
 }
 
-group = "com.wilfred"
-version = "1.0-SNAPSHOT"
+group = "com.wilfred.testremote"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    maven {
+        url = uri("http://nexus.k8s.qa1fdg.net/repository/maven-snapshots/")
+        isAllowInsecureProtocol = true
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("testRemoteA") {
+            from(components.getByName("java"))
+        }
+    }
 }
